c706c2e7344423b458fdf3a1c68f29bfe867f9cd
Jason <genuinemrjay@pm.me>
2019-09-15 14:37:16 -0500

    Created_main

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
-int main() {
-
-  
+int main(void)
+{
+    int failed;
+    Suite *suite;
+    SRunner *runner;
+    
+    suite = check_word_suite();
+    runner = srunner_create(suite);
+    srunner_run_all(runner, CK_NORMAL);
+    failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    return (failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }
