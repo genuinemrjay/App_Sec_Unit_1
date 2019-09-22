3269247f4e976bc7f220c936088d114b52490f54
Jason <genuinemrjay@pm.me>
2019-09-22 00:06:25 -0500

    Update to main.c

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
+/****************************************************************************
+ * main.c
+ *
+ * Application Security, Assignment 1
+ *
+ * Adapted from code written by Ben Halperin
+ ***************************************************************************/
 #include <stdlib.h>
+#include <stdio.h>
+#include "dictionary.h"
+#include <check.h>
 
-int main(void)
+//#define DICTIONARY "wordlist.txt"
+#define TESTDICT "test_worlist.txt"
+
+int main(int argc, char* argv[])
 {
-    int failed;
-    Suite *suite;
-    SRunner *runner;
-    
-    suite = check_word_suite();
-    runner = srunner_create(suite);
-    srunner_run_all(runner, CK_NORMAL);
-    failed = srunner_ntests_failed(runner);
-    srunner_free(runner);
-    return (failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+  // check for correct number of args
+   if (argc != 2 && argc != 3){
+       printf("Usage: spell_check <dictionary_file> <text_file>\n");
+       return 1;
+   }
+  hashmap_t hashtable[HASH_SIZE];
+  load_dictionary(argv[1], hashtable);
+  char *misspelled[MAX_MISSPELLED];
+  FILE *fp = fopen("test1.txt", "r");
+  int num_misspelled = check_words(fp, hashtable, misspelled);
 }
