a6163472db09f72d1c308a39a294d73a36e029c0
Mr Jay <genuinemrjay@pm.me>
2019-09-29 14:25:24 -0500

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -17,8 +17,10 @@ START_TEST(test_dictionary_normal)
 {
     // initialize the hashtable
     hashmap_t hashtable[HASH_SIZE];
+
     // check to load dictionary function
-    ck_assert(load_dictionary(TESTDICT, hashtable));
+    ck_assert(load_dictionary(DICTIONARY, hashtable));
+
     // test words and buckets for table testing
     const char* test_word_1 = "characterizations";
     const char* test_word_2 = "whiz";
@@ -26,7 +28,10 @@ START_TEST(test_dictionary_normal)
     int bucket_1 = hash_function(test_word_1);
     int bucket_2 = hash_function(test_word_2);
     int bucket_3 = hash_function(test_word_3);
-    // check bucket numbers with matching words
+
+    // Here we can test if certain words ended up in certain buckets
+    // to ensure that our load_dictionary works as intended. I leave
+    // this as an exercise.
     for (int i = 0; i < HASH_SIZE;i++) {
          node* cursor = hashtable[i];
          while (cursor != NULL){
@@ -42,25 +47,36 @@ START_TEST(test_dictionary_normal)
              cursor = cursor->next;
          }
      }
-
-    // Here we can test if certain words ended up in certain buckets
-    // to ensure that our load_dictionary works as intended. I leave
-    // this as an exercise.
 }
 END_TEST
 
 START_TEST(test_check_word_normal)
 {
+  // initialize the hashtable
     hashmap_t hashtable[HASH_SIZE];
+
+    // load dictionary function
     load_dictionary(DICTIONARY, hashtable);
+
+    // test for a correct word
     const char* correct_word = "Justice";
-    const char* punctuation_word_2 = "pl.ace";
-    const char* question_mark = "??????ue";
-    const char* too_long = "pneumonoultramicroscopicsilicovolcanoconiosisandstuff";
     ck_assert(check_word(correct_word, hashtable));
+
+    // test for punctuation in the middle of a word
+    const char* punctuation_word_2 = "pl.ace";
     ck_assert(!check_word(punctuation_word_2, hashtable));
+
+    // test for punctuation at the beginning of word
+    const char* question_mark = "??????ue";
     ck_assert(!check_word(question_mark, hashtable));
+
+    // test word word longer than allowed
+    const char* too_long = "pneumonoultramicroscopicsilicovolcanoconiosisandstuff";
     ck_assert(!check_word(too_long, hashtable));
+
+    // test for buffer overflow on word
+    const char* overflow = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    ck_assert(!check_word(overflow, hashtable));
 }
 END_TEST
 
@@ -75,14 +91,25 @@ START_TEST(test_check_words_normal)
     char *misspelled[MAX_MISSPELLED];
     FILE *fp = fopen("test1.txt", "r");
     int num_misspelled = check_words(fp, hashtable, misspelled);
-    //ck_assert_int_eq(num_misspelled ,3);
-    //bool test = strlen(misspelled[0]) == strlen(expected[0]);
-    //int len1 = strlen(misspelled[0]);
-    //int len2 = strlen(expected[0]);
-    //ck_assert_msg(test, "%d!=%d", len1, len2);
-    // ck_assert(strcmp(misspelled[0], expected[0]) == 0);
-    // ck_assert(strcmp(misspelled[1], expected[1]) == 0);
-    // ck_assert(strcmp(misspelled[2], expected[2]) == 0);
+    ck_assert_int_eq(num_misspelled , 3);
+}
+END_TEST
+
+START_TEST(test_check_words)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char* expected[6];
+    expected[0] = "t3st";
+    expected[1] = "t.est";
+    expected[2] = "#tests";
+    expected[3] = "@test";
+    expected[4] = "0f";
+    expected[5] = "Thi$";
+    char *misspelled[MAX_MISSPELLED];
+    FILE *fp = fopen("test_worlist.txt", "r");
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    ck_assert_int_eq(num_misspelled , 6);
 }
 END_TEST
 
@@ -95,6 +122,7 @@ check_word_suite(void)
     check_word_case = tcase_create("Core");
     tcase_add_test(check_word_case, test_dictionary_normal);
     tcase_add_test(check_word_case, test_check_word_normal);
+    tcase_add_test(check_word_case, test_check_words);
     tcase_add_test(check_word_case, test_check_words_normal);
     suite_add_tcase(suite, check_word_case);
 
@@ -115,3 +143,4 @@ main(void)
     srunner_free(runner);
     return (failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }
+
